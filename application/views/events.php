f6af40e4cf99a0353e23e5f1b88ffcf64c42a7d2
Nico Prananta <nico@flutterscape.com>
2011-09-17 15:47:28 +0900

    updated bootstrap.

--- application/views/events.php
+++ application/views/events.php
@@ -1,42 +1,76 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<title>Events</title>
-</head>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.js"></script>
+	<meta name="description" content="">
+    <meta name="author" content="">
 
-<body>
+    <!-- Le HTML5 shim, for IE6-8 support of HTML elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
 
-<div id="posts">
+    <!-- Le styles -->
+    <link href="<?php echo base_url(); ?>css/bootstrap.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>css/docs.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>js/google-code-prettify/prettify.css" rel="stylesheet">
 
-	<p class="heading">Events</p>
+    <!-- Le javascript -->
+    <script src="http://code.jquery.com/jquery-1.5.2.min.js"></script>
+    <script src="http://autobahn.tablesorter.com/jquery.tablesorter.min.js"></script>
+    <script src="<?php echo base_url(); ?>js/google-code-prettify/prettify.js"></script>
+    <script src="<?php echo base_url(); ?>js/application.js"></script>
+    <script src="<?php echo base_url(); ?>js/bootstrap-dropdown.js"></script>
+
+    <!-- Le fav and touch icons -->
+    <link rel="shortcut icon" href="images/favicon.ico">
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+  </head>
+
+<body onload="prettyPrint(); ">
+<?php include 'header.php'; ?>
+
+<section id="body">
+<div id="posts" class="container">
 	<?php foreach ($events as $event) { ?>
-	<div id="event">
-		<h2><?php echo $event->getName(); ?></h2>
-		<h4>By <?php echo $event->getUser()->getName(); ?> on <?php echo $event->getCreatedAt()->format('l F j, Y, g:i a e'); ?></h4>
-		<div id='description'>
-			<?php echo nl2br($event->getDescription()); ?>
-		</div>
-		<div id='place'>
-			<?php echo 'Tempat: '.$event->getPlace(); ?>
-		</div>
-		<div id='time'>
-			<?php setlocale(LC_ALL, 'id_ID.UTF8', 'id_ID.UTF-8', 'id_ID.8859-1', 'id_ID', 'IND.UTF8', 'IND.UTF-8', 'IND.8859-1', 'IND', 'Indonesian.UTF8', 'Indonesian.UTF-8', 'Indonesian.8859-1', 'Indonesian', 'Indonesia', 'id', 'ID'); ?>
-			<?php echo 'Waktu: '.strftime("%A %e %B %Y %r", $event->getTimestart()->getTimestamp()).' sampai '.strftime("%A %e %B %Y %r", $event->getTimeend()->getTimestamp()); ?>
-		</div>
-		<div id='cost'>
-			<?php echo 'Biaya: '.$event->getCost(); ?>
-		</div>
-		<div id='members'>
-			<?php echo 'Panitia :'.implode(',', $event->getMembersArray()); ?>
-		</div>
-		<div id="tags">
-		Tags: <?php echo implode(', ', $event->getTagsNameArray()); ?>
-		</div>
-	</div>
+	<section id="event">
+	<div class="page-header">
+				<h2><?php echo $event->getName(); ?></h2><em> by <?php echo $event->getUser()->getName(); ?> on <?php echo $event->getCreatedAt()->format('l F j, Y, g:i a e'); ?></em>
+				</div>
+			<div id='description' class="row">
+				<div class="span10 columns">
+					<?php echo nl2br($event->getDescription()); ?>
+				</div>
+				<div class="span6 columns">
+					<div id='place'>
+						<?php echo '<strong>Tempat</strong>: '.$event->getPlace(); ?>
+					</div>
+					<div id='time'>
+						<?php setlocale(LC_ALL, 'id_ID.UTF8', 'id_ID.UTF-8', 'id_ID.8859-1', 'id_ID', 'IND.UTF8', 'IND.UTF-8', 'IND.8859-1', 'IND', 'Indonesian.UTF8', 'Indonesian.UTF-8', 'Indonesian.8859-1', 'Indonesian', 'Indonesia', 'id', 'ID'); ?>
+						<?php echo '<strong>Waktu</strong>: '.strftime("%A %e %B %Y %r", $event->getTimestart()->getTimestamp()).' sampai '.strftime("%A %e %B %Y %r", $event->getTimeend()->getTimestamp()); ?>
+					</div>
+					<div id='cost'>
+						<?php echo '<strong>Biaya</strong>: '.$event->getCost(); ?>
+					</div>
+					<div id='members'>
+						<?php echo '<strong>Panitia</strong>: '.implode(',', $event->getMembersArray()); ?>
+					</div>
+					<div id="tags">
+					<strong>Tags</strong>: <?php echo implode(', ', $event->getTagsNameArray()); ?>
+					</div>
+					<div id="by">
+					</div>
+				</div>
+			</div>
+	</section>
+	
+	
+
+	
 	<?php } ?>
 </div>
+</section>
 
 </body>
 </html>
---
+++ application/views/header.php
@@ -0,0 +1,28 @@
+<div class="topbar">
+  <div class="fill">
+	<div class="container">
+	  <h3><a href="/home">PPION</a></h3>
+	  <ul>
+		<li class="active"><a href="/home">Home</a></li>
+		<li><a href="/posts">Posts</a></li>
+		<li><a href="/events">Events</a></li>
+		<li><a href="/members">Members</a></li>
+	  </ul>
+	  
+	<ul class="nav secondary-nav">
+	<form action="">
+		<input type="text" placeholder="Search" />
+	  </form>
+ 	  <li class="dropdown" data-dropdown="dropdown" >
+		<a href="#" class="dropdown-toggle">Setting</a>
+		<ul class="dropdown-menu">
+		  <li><a href="#">Create New Post</a></li>
+		  <li><a href="#">Create New Event</a></li>
+		  <li class="divider"></li>
+		  <li><a href="/logout">Logout</a></li>
+		</ul>
+	  </li>
+	  </ul>
+	</div>
+  </div> <!-- /fill -->
+</div> <!-- /topbar -->
