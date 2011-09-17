684f243a3b1d4dee05a88d78f05100c6862f8641
Nico Prananta <nico@flutterscape.com>
2011-09-17 16:06:47 +0900

    updated ci to 2.0.3

--- system/database/drivers/sqlsrv/sqlsrv_result.php
+++ system/database/drivers/sqlsrv/sqlsrv_result.php
@@ -16,7 +16,7 @@
 // ------------------------------------------------------------------------
 
 /**
- * ODBC Result Class
+ * SQLSRV Result Class
  *
  * This class extends the parent result class: CI_DB_result
  *
@@ -24,7 +24,7 @@
  * @author		ExpressionEngine Dev Team
  * @link		http://codeigniter.com/user_guide/database/
  */
-class CI_DB_odbc_result extends CI_DB_result {
+class CI_DB_sqlsrv_result extends CI_DB_result {
 
 	/**
 	 * Number of rows in the result set
@@ -34,7 +34,7 @@ class CI_DB_odbc_result extends CI_DB_result {
 	 */
 	function num_rows()
 	{
-		return @odbc_num_rows($this->result_id);
+		return @sqlsrv_num_rows($this->result_id);
 	}
 
 	// --------------------------------------------------------------------
@@ -47,7 +47,7 @@ class CI_DB_odbc_result extends CI_DB_result {
 	 */
 	function num_fields()
 	{
-		return @odbc_num_fields($this->result_id);
+		return @sqlsrv_num_fields($this->result_id);
 	}
 
 	// --------------------------------------------------------------------
@@ -63,11 +63,11 @@ class CI_DB_odbc_result extends CI_DB_result {
 	function list_fields()
 	{
 		$field_names = array();
-		for ($i = 0; $i < $this->num_fields(); $i++)
+		foreach(sqlsrv_field_metadata($this->result_id) as $offset => $field)
 		{
-			$field_names[]	= odbc_field_name($this->result_id, $i);
+			$field_names[] = $field['Name'];
 		}
-
+		
 		return $field_names;
 	}
 
@@ -84,18 +84,18 @@ class CI_DB_odbc_result extends CI_DB_result {
 	function field_data()
 	{
 		$retval = array();
-		for ($i = 0; $i < $this->num_fields(); $i++)
+		foreach(sqlsrv_field_metadata($this->result_id) as $offset => $field)
 		{
-			$F				= new stdClass();
-			$F->name		= odbc_field_name($this->result_id, $i);
-			$F->type		= odbc_field_type($this->result_id, $i);
-			$F->max_length	= odbc_field_len($this->result_id, $i);
+			$F 				= new stdClass();
+			$F->name 		= $field['Name'];
+			$F->type 		= $field['Type'];
+			$F->max_length	= $field['Size'];
 			$F->primary_key = 0;
 			$F->default		= '';
-
+			
 			$retval[] = $F;
 		}
-
+		
 		return $retval;
 	}
 
@@ -110,7 +110,7 @@ class CI_DB_odbc_result extends CI_DB_result {
 	{
 		if (is_resource($this->result_id))
 		{
-			odbc_free_result($this->result_id);
+			sqlsrv_free_stmt($this->result_id);
 			$this->result_id = FALSE;
 		}
 	}
@@ -129,7 +129,7 @@ class CI_DB_odbc_result extends CI_DB_result {
 	 */
 	function _data_seek($n = 0)
 	{
-		return FALSE;
+		// Not implemented
 	}
 
 	// --------------------------------------------------------------------
@@ -144,14 +144,7 @@ class CI_DB_odbc_result extends CI_DB_result {
 	 */
 	function _fetch_assoc()
 	{
-		if (function_exists('odbc_fetch_object'))
-		{
-			return odbc_fetch_array($this->result_id);
-		}
-		else
-		{
-			return $this->_odbc_fetch_array($this->result_id);
-		}
+		return sqlsrv_fetch_array($this->result_id, SQLSRV_FETCH_ASSOC);
 	}
 
 	// --------------------------------------------------------------------
@@ -166,63 +159,11 @@ class CI_DB_odbc_result extends CI_DB_result {
 	 */
 	function _fetch_object()
 	{
-		if (function_exists('odbc_fetch_object'))
-		{
-			return odbc_fetch_object($this->result_id);
-		}
-		else
-		{
-			return $this->_odbc_fetch_object($this->result_id);
-		}
-	}
-
-
-	/**
-	 * Result - object
-	 *
-	 * subsititutes the odbc_fetch_object function when
-	 * not available (odbc_fetch_object requires unixODBC)
-	 *
-	 * @access	private
-	 * @return	object
-	 */
-	function _odbc_fetch_object(& $odbc_result) {
-		$rs = array();
-		$rs_obj = FALSE;
-		if (odbc_fetch_into($odbc_result, $rs)) {
-			foreach ($rs as $k=>$v) {
-				$field_name= odbc_field_name($odbc_result, $k+1);
-				$rs_obj->$field_name = $v;
-			}
-		}
-		return $rs_obj;
-	}
-
-
-	/**
-	 * Result - array
-	 *
-	 * subsititutes the odbc_fetch_array function when
-	 * not available (odbc_fetch_array requires unixODBC)
-	 *
-	 * @access	private
-	 * @return	array
-	 */
-	function _odbc_fetch_array(& $odbc_result) {
-		$rs = array();
-		$rs_assoc = FALSE;
-		if (odbc_fetch_into($odbc_result, $rs)) {
-			$rs_assoc=array();
-			foreach ($rs as $k=>$v) {
-				$field_name= odbc_field_name($odbc_result, $k+1);
-				$rs_assoc[$field_name] = $v;
-			}
-		}
-		return $rs_assoc;
+		return sqlsrv_fetch_object($this->result_id);
 	}
 
 }
 
 
-/* End of file odbc_result.php */
-/* Location: ./system/database/drivers/odbc/odbc_result.php */
+/* End of file mssql_result.php */
+/* Location: ./system/database/drivers/mssql/mssql_result.php */
