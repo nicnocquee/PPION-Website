24b79a3a9c64c4f9c58082d5ae651590a1d0b01d
Nico Prananta <nico@flutterscape.com>
2011-09-20 00:44:37 +0900

    tweak dmmy sql

--- sqls/ppion-dummy.sql
+++ sqls/ppion-dummy.sql
@@ -1,11 +1,15 @@
-
---
--- Dumping data for table `comments`
---
-
-INSERT INTO `comments` VALUES(1, 20, 4, 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec suscipit auctor mi vel volutpat. Nam suscipit lacus nisl. Suspendisse sagittis dui a felis pulvinar pretium interdum neque bibendum. Fusce varius vehicula auctor. Fusce mollis tincidunt leo, ullamcorper placerat lacus ultrices a. Vestibulum tincidunt varius augue vitae luctus. Aenean pretium vulputate massa non elementum. Donec cursus lacus ac arcu condimentum posuere quis vitae nisl. Nulla vulputate nisl at enim tincidunt eget molestie arcu convallis. Quisque sed ante in arcu ullamcorper consectetur vel vel libero. Nullam eget purus eu purus pretium fermentum id nec arcu. Maecenas tincidunt, ante varius auctor pretium, nisl tortor ultricies nulla, et viverra nisl nibh sit amet mi. Sed metus arcu, consectetur non sodales et, aliquet in ante. Nam luctus, purus non blandit posuere, velit mi viverra ipsum, id posuere est mauris ac est. Ut arcu lorem, elementum nec blandit sed, ultrices vel elit. Nu', '2011-09-17 21:39:33', NULL);
-INSERT INTO `comments` VALUES(2, 20, 10, 'I agree!', '2011-09-17 21:39:33', NULL);
-INSERT INTO `comments` VALUES(3, 19, 2, 'wowww!', '2011-09-17 21:40:02', NULL);
+INSERT INTO `users` VALUES(1, '2@ppion.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'Dua', 'Bandung', 'Osaka City University', '2008-04-01', '1985-01-21', 1, 'M', 'Islam', 'Lorem ipsum bla bla bla', 'Institut Teknologi Bandung', 'Teknik Elektro', 2006, 'Osaka City University', 'Communication System', 2011, 'Osaka City University', 'Communication System', 2014, 'iOS development', 0, 'Web developer', '2011-08-05 18:52:38', NULL);
+INSERT INTO `users` VALUES(2, '3@ppion.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'Tiga', 'Bandung', 'Osaka City University', '2008-04-01', '1985-01-21', 1, 'M', 'Islam', 'Lorem ipsum bla bla bla', 'Institut Teknologi Bandung', 'Teknik Elektro', 2006, 'Osaka City University', 'Communication System', 2011, 'Osaka City University', 'Communication System', 2014, 'iOS development', 0, 'Web developer', '2011-08-05 18:52:38', NULL);
+INSERT INTO `users` VALUES(3, '4@ppion.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'Empat', 'Bandung', 'Osaka City University', '2008-04-01', '1985-01-21', 1, 'M', 'Islam', 'Lorem ipsum bla bla bla', 'Institut Teknologi Bandung', 'Teknik Elektro', 2006, 'Osaka City University', 'Communication System', 2011, 'Osaka City University', 'Communication System', 2014, 'iOS development', 0, 'Web developer', '2011-08-05 18:52:38', NULL);
+INSERT INTO `users` VALUES(4, 'nico@flutterscape.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'NicoPrananta', 'Bandung', 'Flutterscape', '2008-04-01', '1985-01-21', 0, '0', 'Islam', 'about.me/nico.prananta', 'Institut Teknologi Bandung', 'Teknik Elektro', 2002, 'Osaka City University', 'Teknik Elektro', 2011, '', '', 0, 'iOS dev and web dev', 0, '', '2011-08-06 01:17:46', NULL);
+INSERT INTO `users` VALUES(5, 'nico@com.com', '48cccca3bab2ad18832233ee8dff1b0b', 'Nico Prananta', 'Bandung', 'Flutterscape', '2008-04-01', '1985-01-21', 0, '1', 'Islam', 'bio bio bio', 'Institut Teknologi Bandung', 'Teknik Elektro', 2002, 'Osaka City University', 'Teknik Elektro', 2011, '', '', 0, 'iOS dev and web dev', 1, '', '2011-08-06 01:22:12', NULL);
+INSERT INTO `users` VALUES(6, 'nico@com.comm', 'ddbac82a201a23d4737376af67b8e79d', 'Nico Prananta', 'Bandung', 'Flutterscape', '2008-04-01', '1985-01-21', 0, '0', 'Islam', 'bio bio', 'Institut Teknologi Bandung', 'Teknik Elektro', 2002, 'Osaka City University', 'Teknik Elektro', 2011, '', '', 0, 'iOS dev and web dev', 0, '', '2011-08-06 03:34:31', NULL);
+INSERT INTO `users` VALUES(7, 'nico@com.commm', '343b1c4a3ea721b2d640fc8700db0f36', 'Nico Prananta', 'Bandung', 'Flutterscape', '2008-04-01', '1985-01-21', 0, '0', 'Islam', 'bio bio', 'Institut Teknologi Bandung', 'Teknik Elektro', 2002, 'Osaka City University', 'Teknik Elektro', 2011, '', '', 0, 'iOS dev and web dev', 0, '', '2011-08-06 04:20:11', NULL);
+INSERT INTO `users` VALUES(8, 'nico@com.commmm', 'e10adc3949ba59abbe56e057f20f883e', 'Nico Prananta', 'Bandung', 'Flutterscape', '2008-04-01', '1985-01-21', 0, '0', 'Islam', 'bio bio', 'Institut Teknologi Bandung', 'Teknik Elektro', 2002, 'Osaka City University', 'Teknik Elektro', 2011, 'osaka', '93931', 9931, 'iOS dev and web dev', 0, '', '2011-08-06 04:23:39', NULL);
+INSERT INTO `users` VALUES(9, 'nio@com.commmm', '0b4e7a0e5fe84ad35fb5f95b9ceeac79', 'Nico Prananta', 'Bandung', 'Flutterscape', '2008-04-01', '1985-01-21', 0, '0', 'Islam', 'bhhfs sfgsfgs', 'Institut Teknologi Bandung', 'Teknik Elektro', 2002, 'Osaka City University', 'Teknik Elektro', 2011, 'osaka', '93931', 9931, 'iOS dev and web dev', 0, 'ngga ada', '2011-08-06 04:25:41', NULL);
+INSERT INTO `users` VALUES(10, 'nicco@com.commmm', 'e10adc3949ba59abbe56e057f20f883e', 'Nico Prananta', 'Bandung', 'Flutterscape', '2008-04-01', '1985-01-21', 0, '0', 'Islam', 'bio bio', 'Institut Teknologi Bandung', 'Teknik Elektro', 2002, 'Osaka City University', 'Teknik Elektro', 2011, '', '', 0, 'iOS dev and web dev', 0, 'ngga ada', '2011-08-06 09:47:57', NULL);
+INSERT INTO `users` VALUES(11, 'niiicco@com.commmm', '343b1c4a3ea721b2d640fc8700db0f36', 'Nico Prananta', 'Bandung', 'Flutterscape', '2008-04-01', '1985-01-21', 0, '0', 'Islam', 'bio bio', 'Institut Teknologi Bandung', 'Teknik Elektro', 2002, 'Osaka City University', 'Teknik Elektro', 2011, '', '', 0, 'iOS dev and web dev', 0, 'ngga ada', '2011-08-06 09:49:46', NULL);
+INSERT INTO `users` VALUES(12, 'nico@ppion.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'Nico Prananta', 'Bandung', 'Flutterscape', '2008-04-01', '1985-01-21', 0, '0', 'Islam', 'about.me/nico.prananta', 'Institut Teknologi Bandung', 'Teknik Elektro', 2006, 'Osaka City University', 'Teknik Elektro', 2011, '', '', 0, 'iOS dev and web dev', 0, 'Web developer', '2011-08-11 22:34:49', NULL);
 
 -- --------------------------------------------------------
 
@@ -27,16 +31,6 @@ INSERT INTO `contacts` VALUES(8, 12, 'nicnocquee', '5', 0, '2011-08-11 22:34:49'
 
 
 
---
--- Dumping data for table `eventmembers`
---
-
-INSERT INTO `eventmembers` VALUES(1, 1, 1, 'leader', 'lead', '2011-08-05 18:52:38', NULL);
-INSERT INTO `eventmembers` VALUES(2, 1, 2, 'transportation', 'taking care of cars', '2011-08-05 18:52:38', NULL);
-
--- --------------------------------------------------------
-
-
 
 --
 -- Dumping data for table `events`
@@ -45,6 +39,16 @@ INSERT INTO `eventmembers` VALUES(2, 1, 2, 'transportation', 'taking care of car
 INSERT INTO `events` VALUES(1, 1, 'Hanami', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum pulvinar augue ultrices ante ornare dictum. Aenean id lectus quam. Sed feugiat nibh quis nibh eleifend id accumsan felis consequat. Aenean suscipit imperdiet tortor, nec cursus justo imperdiet nec. Phasellus id lacus ligula, at sollicitudin odio. Proin facilisis, velit a malesuada sagittis, eros lorem lacinia orci, laoreet blandit enim mauris et orci. Curabitur ac nulla fringilla libero accumsan ultricies. Cras ultrices euismod luctus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In urna eros, ullamcorper at malesuada at, luctus et ipsum. Nam id ante vitae odio lobortis elementum a id justo. Ut sapien ante, scelerisque in ultrices at, lobortis quis risus. Proin pulvinar odio id ante euismod blandit faucibus sed massa.  Proin commodo, nibh aliquet eleifend tristique, lacus leo porta nunc, ac ullamcorper justo mi non tellus. Vivamus consequat sagittis nisl. Nunc eget malesuada leo. Integer vulputate eleifend ', 'Osaka Castle', '2011-08-21 00:00:00', 3000, '2011-09-01 00:00:00', '2011-09-01 00:00:00', '', '2011-08-05 18:52:38', NULL);
 INSERT INTO `events` VALUES(2, 1, 'New year', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum pulvinar augue ultrices ante ornare dictum. Aenean id lectus quam. Sed feugiat nibh quis nibh eleifend id accumsan felis consequat. Aenean suscipit imperdiet tortor, nec cursus justo imperdiet nec. Phasellus id lacus ligula, at sollicitudin odio. Proin facilisis, velit a malesuada sagittis, eros lorem lacinia orci, laoreet blandit enim mauris et orci. Curabitur ac nulla fringilla libero accumsan ultricies. Cras ultrices euismod luctus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In urna eros, ullamcorper at malesuada at, luctus et ipsum. Nam id ante vitae odio lobortis elementum a id justo. Ut sapien ante, scelerisque in ultrices at, lobortis quis risus. Proin pulvinar odio id ante euismod blandit faucibus sed massa.  Proin commodo, nibh aliquet eleifend tristique, lacus leo porta nunc, ac ullamcorper justo mi non tellus. Vivamus consequat sagittis nisl. Nunc eget malesuada leo. Integer vulputate eleifend ', 'Onohara', '2011-12-21 00:00:00', 3000, '2011-12-31 00:00:00', '2012-01-01 00:00:00', 'Single only', '2011-08-05 18:52:38', NULL);
 INSERT INTO `events` VALUES(3, 1, 'Ngabuburit', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum pulvinar augue ultrices ante ornare dictum. Aenean id lectus quam. Sed feugiat nibh quis nibh eleifend id accumsan felis consequat. Aenean suscipit imperdiet tortor, nec cursus justo imperdiet nec. Phasellus id lacus ligula, at sollicitudin odio. Proin facilisis, velit a malesuada sagittis, eros lorem lacinia orci, laoreet blandit enim mauris et orci. Curabitur ac nulla fringilla libero accumsan ultricies. Cras ultrices euismod luctus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In urna eros, ullamcorper at malesuada at, luctus et ipsum. Nam id ante vitae odio lobortis elementum a id justo. Ut sapien ante, scelerisque in ultrices at, lobortis quis risus. Proin pulvinar odio id ante euismod blandit faucibus sed massa.  Proin commodo, nibh aliquet eleifend tristique, lacus leo porta nunc, ac ullamcorper justo mi non tellus. Vivamus consequat sagittis nisl. Nunc eget malesuada leo. Integer vulputate eleifend ', 'Onohara', '2010-09-22 00:00:00', 1000, '2010-03-21 14:30:00', '2001-01-03 11:20:00', '', '2011-08-10 23:17:21', NULL);
+
+-- --------------------------------------------------------
+
+
+--
+-- Dumping data for table `eventmembers`
+--
+
+INSERT INTO `eventmembers` VALUES(1, 1, 1, 'leader', 'lead', '2011-08-05 18:52:38', NULL);
+INSERT INTO `eventmembers` VALUES(2, 1, 2, 'transportation', 'taking care of cars', '2011-08-05 18:52:38', NULL);
 
 -- --------------------------------------------------------
 
@@ -123,6 +127,18 @@ INSERT INTO `posts` VALUES(20, 1, 'Article 3', 'Lorem ipsum dolor sit amet, cons
 -- --------------------------------------------------------
 
 
+INSERT INTO `tags` VALUES(67, 'hello', '2011-08-07 12:13:39', NULL);
+INSERT INTO `tags` VALUES(68, 'world', '2011-08-07 12:13:39', NULL);
+INSERT INTO `tags` VALUES(69, 'japan', '2011-08-07 12:13:39', NULL);
+INSERT INTO `tags` VALUES(70, 'sakura', '2011-08-07 12:15:17', NULL);
+INSERT INTO `tags` VALUES(71, 'summer', '2011-08-07 12:15:17', NULL);
+INSERT INTO `tags` VALUES(72, 'lorem', '2011-08-07 12:17:31', NULL);
+INSERT INTO `tags` VALUES(73, 'ipsum', '2011-08-07 12:17:31', NULL);
+
+-- --------------------------------------------------------
+
+
+--
 
 --
 -- Dumping data for table `posts_tags`
@@ -140,6 +156,14 @@ INSERT INTO `posts_tags` VALUES(20, 73);
 
 -- --------------------------------------------------------
 
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` VALUES(1, 20, 4, 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec suscipit auctor mi vel volutpat. Nam suscipit lacus nisl. Suspendisse sagittis dui a felis pulvinar pretium interdum neque bibendum. Fusce varius vehicula auctor. Fusce mollis tincidunt leo, ullamcorper placerat lacus ultrices a. Vestibulum tincidunt varius augue vitae luctus. Aenean pretium vulputate massa non elementum. Donec cursus lacus ac arcu condimentum posuere quis vitae nisl. Nulla vulputate nisl at enim tincidunt eget molestie arcu convallis. Quisque sed ante in arcu ullamcorper consectetur vel vel libero. Nullam eget purus eu purus pretium fermentum id nec arcu. Maecenas tincidunt, ante varius auctor pretium, nisl tortor ultricies nulla, et viverra nisl nibh sit amet mi. Sed metus arcu, consectetur non sodales et, aliquet in ante. Nam luctus, purus non blandit posuere, velit mi viverra ipsum, id posuere est mauris ac est. Ut arcu lorem, elementum nec blandit sed, ultrices vel elit. Nu', '2011-09-17 21:39:33', NULL);
+INSERT INTO `comments` VALUES(2, 20, 10, 'I agree!', '2011-09-17 21:39:33', NULL);
+INSERT INTO `comments` VALUES(3, 19, 2, 'wowww!', '2011-09-17 21:40:02', NULL);
+
 
 
 --
@@ -152,16 +176,6 @@ INSERT INTO `role_data` VALUES(7, 'user');
 
 -- --------------------------------------------------------
 
-
-
---
--- Dumping data for table `role_perms`
---
-
-INSERT INTO `role_perms` VALUES(3, 5, 39, 1, '2011-08-13 23:11:01');
-INSERT INTO `role_perms` VALUES(4, 5, 40, 1, '2011-08-13 23:11:01');
-INSERT INTO `role_perms` VALUES(8, 6, 40, 0, '2011-08-13 23:51:06');
-INSERT INTO `role_perms` VALUES(7, 6, 39, 0, '2011-08-13 23:51:06');
 
 -- --------------------------------------------------------
 
@@ -183,33 +197,10 @@ INSERT INTO `rsvps` VALUES(2, 1, 2, 1, '2011-08-05 18:52:38', NULL);
 -- Dumping data for table `tags`
 --
 
-INSERT INTO `tags` VALUES(67, 'hello', '2011-08-07 12:13:39', NULL);
-INSERT INTO `tags` VALUES(68, 'world', '2011-08-07 12:13:39', NULL);
-INSERT INTO `tags` VALUES(69, 'japan', '2011-08-07 12:13:39', NULL);
-INSERT INTO `tags` VALUES(70, 'sakura', '2011-08-07 12:15:17', NULL);
-INSERT INTO `tags` VALUES(71, 'summer', '2011-08-07 12:15:17', NULL);
-INSERT INTO `tags` VALUES(72, 'lorem', '2011-08-07 12:17:31', NULL);
-INSERT INTO `tags` VALUES(73, 'ipsum', '2011-08-07 12:17:31', NULL);
-
--- --------------------------------------------------------
-
-
---
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` VALUES(1, '2@ppion.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'Dua', 'Bandung', 'Osaka City University', '2008-04-01', '1985-01-21', 1, 'M', 'Islam', 'Lorem ipsum bla bla bla', 'Institut Teknologi Bandung', 'Teknik Elektro', 2006, 'Osaka City University', 'Communication System', 2011, 'Osaka City University', 'Communication System', 2014, 'iOS development', 0, 'Web developer', '2011-08-05 18:52:38', NULL);
-INSERT INTO `users` VALUES(2, '3@ppion.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'Tiga', 'Bandung', 'Osaka City University', '2008-04-01', '1985-01-21', 1, 'M', 'Islam', 'Lorem ipsum bla bla bla', 'Institut Teknologi Bandung', 'Teknik Elektro', 2006, 'Osaka City University', 'Communication System', 2011, 'Osaka City University', 'Communication System', 2014, 'iOS development', 0, 'Web developer', '2011-08-05 18:52:38', NULL);
-INSERT INTO `users` VALUES(3, '4@ppion.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'Empat', 'Bandung', 'Osaka City University', '2008-04-01', '1985-01-21', 1, 'M', 'Islam', 'Lorem ipsum bla bla bla', 'Institut Teknologi Bandung', 'Teknik Elektro', 2006, 'Osaka City University', 'Communication System', 2011, 'Osaka City University', 'Communication System', 2014, 'iOS development', 0, 'Web developer', '2011-08-05 18:52:38', NULL);
-INSERT INTO `users` VALUES(4, 'nico@flutterscape.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'NicoPrananta', 'Bandung', 'Flutterscape', '2008-04-01', '1985-01-21', 0, '0', 'Islam', 'about.me/nico.prananta', 'Institut Teknologi Bandung', 'Teknik Elektro', 2002, 'Osaka City University', 'Teknik Elektro', 2011, '', '', 0, 'iOS dev and web dev', 0, '', '2011-08-06 01:17:46', NULL);
-INSERT INTO `users` VALUES(5, 'nico@com.com', '48cccca3bab2ad18832233ee8dff1b0b', 'Nico Prananta', 'Bandung', 'Flutterscape', '2008-04-01', '1985-01-21', 0, '1', 'Islam', 'bio bio bio', 'Institut Teknologi Bandung', 'Teknik Elektro', 2002, 'Osaka City University', 'Teknik Elektro', 2011, '', '', 0, 'iOS dev and web dev', 1, '', '2011-08-06 01:22:12', NULL);
-INSERT INTO `users` VALUES(6, 'nico@com.comm', 'ddbac82a201a23d4737376af67b8e79d', 'Nico Prananta', 'Bandung', 'Flutterscape', '2008-04-01', '1985-01-21', 0, '0', 'Islam', 'bio bio', 'Institut Teknologi Bandung', 'Teknik Elektro', 2002, 'Osaka City University', 'Teknik Elektro', 2011, '', '', 0, 'iOS dev and web dev', 0, '', '2011-08-06 03:34:31', NULL);
-INSERT INTO `users` VALUES(7, 'nico@com.commm', '343b1c4a3ea721b2d640fc8700db0f36', 'Nico Prananta', 'Bandung', 'Flutterscape', '2008-04-01', '1985-01-21', 0, '0', 'Islam', 'bio bio', 'Institut Teknologi Bandung', 'Teknik Elektro', 2002, 'Osaka City University', 'Teknik Elektro', 2011, '', '', 0, 'iOS dev and web dev', 0, '', '2011-08-06 04:20:11', NULL);
-INSERT INTO `users` VALUES(8, 'nico@com.commmm', 'e10adc3949ba59abbe56e057f20f883e', 'Nico Prananta', 'Bandung', 'Flutterscape', '2008-04-01', '1985-01-21', 0, '0', 'Islam', 'bio bio', 'Institut Teknologi Bandung', 'Teknik Elektro', 2002, 'Osaka City University', 'Teknik Elektro', 2011, 'osaka', '93931', 9931, 'iOS dev and web dev', 0, '', '2011-08-06 04:23:39', NULL);
-INSERT INTO `users` VALUES(9, 'nio@com.commmm', '0b4e7a0e5fe84ad35fb5f95b9ceeac79', 'Nico Prananta', 'Bandung', 'Flutterscape', '2008-04-01', '1985-01-21', 0, '0', 'Islam', 'bhhfs sfgsfgs', 'Institut Teknologi Bandung', 'Teknik Elektro', 2002, 'Osaka City University', 'Teknik Elektro', 2011, 'osaka', '93931', 9931, 'iOS dev and web dev', 0, 'ngga ada', '2011-08-06 04:25:41', NULL);
-INSERT INTO `users` VALUES(10, 'nicco@com.commmm', 'e10adc3949ba59abbe56e057f20f883e', 'Nico Prananta', 'Bandung', 'Flutterscape', '2008-04-01', '1985-01-21', 0, '0', 'Islam', 'bio bio', 'Institut Teknologi Bandung', 'Teknik Elektro', 2002, 'Osaka City University', 'Teknik Elektro', 2011, '', '', 0, 'iOS dev and web dev', 0, 'ngga ada', '2011-08-06 09:47:57', NULL);
-INSERT INTO `users` VALUES(11, 'niiicco@com.commmm', '343b1c4a3ea721b2d640fc8700db0f36', 'Nico Prananta', 'Bandung', 'Flutterscape', '2008-04-01', '1985-01-21', 0, '0', 'Islam', 'bio bio', 'Institut Teknologi Bandung', 'Teknik Elektro', 2002, 'Osaka City University', 'Teknik Elektro', 2011, '', '', 0, 'iOS dev and web dev', 0, 'ngga ada', '2011-08-06 09:49:46', NULL);
-INSERT INTO `users` VALUES(12, 'nico@ppion.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'Nico Prananta', 'Bandung', 'Flutterscape', '2008-04-01', '1985-01-21', 0, '0', 'Islam', 'about.me/nico.prananta', 'Institut Teknologi Bandung', 'Teknik Elektro', 2006, 'Osaka City University', 'Teknik Elektro', 2011, '', '', 0, 'iOS dev and web dev', 0, 'Web developer', '2011-08-11 22:34:49', NULL);
+
 
 -- --------------------------------------------------------
 
