60a043a661b7736bff2e74fe47924ec191c96b7e
Nico Prananta <nico@flutterscape.com>
2011-09-20 00:39:52 +0900

    tweak dmmy sql

--- sqls/ppion-dummy.sql
+++ sqls/ppion-dummy.sql
@@ -1,41 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 3.2.5
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Sep 20, 2011 at 12:34 AM
--- Server version: 5.1.44
--- PHP Version: 5.3.2
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `ppion`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `comments`
---
-
-CREATE TABLE `comments` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `post_id` int(11) DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `content` varchar(1000) NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `comments_post_id_idx` (`post_id`),
-  KEY `comments_user_id_idx` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Dumping data for table `comments`
@@ -47,21 +9,6 @@ INSERT INTO `comments` VALUES(3, 19, 2, 'wowww!', '2011-09-17 21:40:02', NULL);
 
 -- --------------------------------------------------------
 
---
--- Table structure for table `contacts`
---
-
-CREATE TABLE `contacts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) DEFAULT NULL,
-  `address` varchar(200) NOT NULL,
-  `type` varchar(64) NOT NULL,
-  `visibility` tinyint(1) NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `contacts_user_id_idx` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
 
 --
 -- Dumping data for table `contacts`
@@ -78,22 +25,7 @@ INSERT INTO `contacts` VALUES(8, 12, 'nicnocquee', '5', 0, '2011-08-11 22:34:49'
 
 -- --------------------------------------------------------
 
---
--- Table structure for table `eventmembers`
---
 
-CREATE TABLE `eventmembers` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `event_id` int(11) DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `position` varchar(200) NOT NULL,
-  `responsibility` varchar(200) NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `eventmembers_event_id_idx` (`event_id`),
-  KEY `eventmembers_user_id_idx` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `eventmembers`
@@ -104,26 +36,7 @@ INSERT INTO `eventmembers` VALUES(2, 1, 2, 'transportation', 'taking care of car
 
 -- --------------------------------------------------------
 
---
--- Table structure for table `events`
---
 
-CREATE TABLE `events` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) DEFAULT NULL,
-  `name` varchar(200) NOT NULL,
-  `description` varchar(1000) NOT NULL,
-  `place` varchar(200) NOT NULL,
-  `deadline` datetime NOT NULL,
-  `cost` int(11) DEFAULT NULL,
-  `timeStart` datetime NOT NULL,
-  `timeEnd` datetime NOT NULL,
-  `limitation` varchar(1000) NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `events_user_id_idx` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Dumping data for table `events`
@@ -135,17 +48,7 @@ INSERT INTO `events` VALUES(3, 1, 'Ngabuburit', 'Lorem ipsum dolor sit amet, con
 
 -- --------------------------------------------------------
 
---
--- Table structure for table `events_tags`
---
 
-CREATE TABLE `events_tags` (
-  `event_id` int(11) NOT NULL,
-  `eventtag_id` int(11) NOT NULL,
-  PRIMARY KEY (`event_id`,`eventtag_id`),
-  KEY `events_tags_event_id_idx` (`event_id`),
-  KEY `events_tags_eventtag_id_idx` (`eventtag_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `events_tags`
@@ -161,19 +64,7 @@ INSERT INTO `events_tags` VALUES(3, 7);
 
 -- --------------------------------------------------------
 
---
--- Table structure for table `eventtags`
---
 
-CREATE TABLE `eventtags` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(200) NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
-
---
 -- Dumping data for table `eventtags`
 --
 
@@ -187,19 +78,7 @@ INSERT INTO `eventtags` VALUES(7, 'japan', '2011-08-10 23:11:20', NULL);
 
 -- --------------------------------------------------------
 
---
--- Table structure for table `expertises`
---
 
-CREATE TABLE `expertises` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) DEFAULT NULL,
-  `name` varchar(200) NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `expertises_user_id_idx` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `expertises`
@@ -210,21 +89,6 @@ INSERT INTO `expertises` VALUES(2, 1, 'math', '2011-08-05 18:52:38', NULL);
 
 -- --------------------------------------------------------
 
---
--- Table structure for table `family`
---
-
-CREATE TABLE `family` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) DEFAULT NULL,
-  `member` int(11) NOT NULL,
-  `relationship` varchar(64) NOT NULL,
-  `confirmed` tinyint(1) NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `family_user_id_idx` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `family`
@@ -235,62 +99,7 @@ INSERT INTO `family` VALUES(2, 1, 3, 'daughter', 1, '2011-08-05 18:52:38', NULL)
 
 -- --------------------------------------------------------
 
---
--- Table structure for table `likes`
---
 
-CREATE TABLE `likes` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user` int(11) DEFAULT NULL,
-  `post` int(11) DEFAULT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `likes_user_idx` (`user`),
-  KEY `likes_post_idx` (`post`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
---
--- Dumping data for table `likes`
---
-
-
--- --------------------------------------------------------
-
---
--- Table structure for table `perm_data`
---
-
-CREATE TABLE `perm_data` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `permKey` varchar(30) NOT NULL,
-  `permName` varchar(30) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `permKey` (`permKey`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=41 ;
-
---
--- Dumping data for table `perm_data`
---
-
-
--- --------------------------------------------------------
-
---
--- Table structure for table `photos`
---
-
-CREATE TABLE `photos` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) DEFAULT NULL,
-  `url` varchar(200) NOT NULL,
-  `direction` varchar(20) NOT NULL,
-  `profile` tinyint(1) DEFAULT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `photos_user_id_idx` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `photos`
@@ -301,21 +110,7 @@ INSERT INTO `photos` VALUES(2, 1, 'http://something2.jpg', 'right', 1, '2011-08-
 
 -- --------------------------------------------------------
 
---
--- Table structure for table `posts`
---
 
-CREATE TABLE `posts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user` int(11) DEFAULT NULL,
-  `title` varchar(200) NOT NULL,
-  `content` varchar(3000) NOT NULL,
-  `flag` varchar(200) DEFAULT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `posts_user_idx` (`user`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=21 ;
 
 --
 -- Dumping data for table `posts`
@@ -327,17 +122,7 @@ INSERT INTO `posts` VALUES(20, 1, 'Article 3', 'Lorem ipsum dolor sit amet, cons
 
 -- --------------------------------------------------------
 
---
--- Table structure for table `posts_tags`
---
 
-CREATE TABLE `posts_tags` (
-  `post_id` int(11) NOT NULL,
-  `tag_id` int(11) NOT NULL,
-  PRIMARY KEY (`post_id`,`tag_id`),
-  KEY `posts_tags_post_id_idx` (`post_id`),
-  KEY `posts_tags_tag_id_idx` (`tag_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `posts_tags`
@@ -355,16 +140,7 @@ INSERT INTO `posts_tags` VALUES(20, 73);
 
 -- --------------------------------------------------------
 
---
--- Table structure for table `role_data`
---
 
-CREATE TABLE `role_data` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `roleName` varchar(20) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `roleName` (`roleName`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
 --
 -- Dumping data for table `role_data`
@@ -376,19 +152,7 @@ INSERT INTO `role_data` VALUES(7, 'user');
 
 -- --------------------------------------------------------
 
---
--- Table structure for table `role_perms`
---
 
-CREATE TABLE `role_perms` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `roleID` bigint(20) NOT NULL,
-  `permID` bigint(20) NOT NULL,
-  `value` tinyint(1) NOT NULL DEFAULT '0',
-  `addDate` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `roleID_2` (`roleID`,`permID`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
 
 --
 -- Dumping data for table `role_perms`
@@ -401,21 +165,6 @@ INSERT INTO `role_perms` VALUES(7, 6, 39, 0, '2011-08-13 23:51:06');
 
 -- --------------------------------------------------------
 
---
--- Table structure for table `rsvps`
---
-
-CREATE TABLE `rsvps` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `event_id` int(11) DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `answer` int(11) NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `rsvps_event_id_idx` (`event_id`),
-  KEY `rsvps_user_id_idx` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `rsvps`
@@ -429,14 +178,6 @@ INSERT INTO `rsvps` VALUES(2, 1, 2, 1, '2011-08-05 18:52:38', NULL);
 --
 -- Table structure for table `tags`
 --
-
-CREATE TABLE `tags` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(200) NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=74 ;
 
 --
 -- Dumping data for table `tags`
@@ -452,40 +193,6 @@ INSERT INTO `tags` VALUES(73, 'ipsum', '2011-08-07 12:17:31', NULL);
 
 -- --------------------------------------------------------
 
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(64) NOT NULL,
-  `password` varchar(64) NOT NULL,
-  `name` varchar(64) NOT NULL,
-  `hometown` varchar(64) NOT NULL,
-  `affiliation` varchar(64) NOT NULL,
-  `arrival_date` date NOT NULL,
-  `birthday` date DEFAULT NULL,
-  `marriage_status` tinyint(1) NOT NULL,
-  `gender` varchar(1) DEFAULT NULL,
-  `religion` varchar(20) DEFAULT NULL,
-  `introduction` varchar(140) DEFAULT NULL,
-  `undergrad_university` varchar(64) NOT NULL,
-  `undergrad_department` varchar(64) DEFAULT NULL,
-  `undergrad_graduation_year` int(11) DEFAULT NULL,
-  `master_university` varchar(64) DEFAULT NULL,
-  `master_department` varchar(64) DEFAULT NULL,
-  `master_graduation_year` int(11) DEFAULT NULL,
-  `phd_university` varchar(64) DEFAULT NULL,
-  `phd_department` varchar(64) DEFAULT NULL,
-  `phd_graduation_year` int(11) DEFAULT NULL,
-  `informal_skill` varchar(64) DEFAULT NULL,
-  `left_the_country` tinyint(1) NOT NULL,
-  `position` varchar(32) DEFAULT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `users_email_uniq` (`email`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
 
 --
 -- Dumping data for table `users`
@@ -506,37 +213,7 @@ INSERT INTO `users` VALUES(12, 'nico@ppion.com', '5f4dcc3b5aa765d61d8327deb882cf
 
 -- --------------------------------------------------------
 
---
--- Table structure for table `user_perms`
---
 
-CREATE TABLE `user_perms` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `userID` bigint(20) NOT NULL,
-  `permID` bigint(20) NOT NULL,
-  `value` tinyint(1) NOT NULL DEFAULT '0',
-  `addDate` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `userID` (`userID`,`permID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
---
--- Dumping data for table `user_perms`
---
-
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user_roles`
---
-
-CREATE TABLE `user_roles` (
-  `userID` bigint(20) NOT NULL,
-  `roleID` bigint(20) NOT NULL,
-  `addDate` datetime NOT NULL,
-  UNIQUE KEY `userID` (`userID`,`roleID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user_roles`
@@ -545,84 +222,3 @@ CREATE TABLE `user_roles` (
 INSERT INTO `user_roles` VALUES(12, 5, '2011-08-13 23:09:54');
 INSERT INTO `user_roles` VALUES(4, 7, '2011-08-13 23:51:57');
 
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `comments`
---
-ALTER TABLE `comments`
-  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`),
-  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
-
---
--- Constraints for table `contacts`
---
-ALTER TABLE `contacts`
-  ADD CONSTRAINT `contacts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
-
---
--- Constraints for table `eventmembers`
---
-ALTER TABLE `eventmembers`
-  ADD CONSTRAINT `eventmembers_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `events` (`id`),
-  ADD CONSTRAINT `eventmembers_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
-
---
--- Constraints for table `events`
---
-ALTER TABLE `events`
-  ADD CONSTRAINT `events_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
-
---
--- Constraints for table `events_tags`
---
-ALTER TABLE `events_tags`
-  ADD CONSTRAINT `events_tags_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `events` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `events_tags_ibfk_2` FOREIGN KEY (`eventtag_id`) REFERENCES `eventtags` (`id`) ON DELETE CASCADE;
-
---
--- Constraints for table `expertises`
---
-ALTER TABLE `expertises`
-  ADD CONSTRAINT `expertises_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
-
---
--- Constraints for table `family`
---
-ALTER TABLE `family`
-  ADD CONSTRAINT `family_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
-
---
--- Constraints for table `likes`
---
-ALTER TABLE `likes`
-  ADD CONSTRAINT `likes_ibfk_1` FOREIGN KEY (`user`) REFERENCES `users` (`id`),
-  ADD CONSTRAINT `likes_ibfk_2` FOREIGN KEY (`post`) REFERENCES `posts` (`id`);
-
---
--- Constraints for table `photos`
---
-ALTER TABLE `photos`
-  ADD CONSTRAINT `photos_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
-
---
--- Constraints for table `posts`
---
-ALTER TABLE `posts`
-  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user`) REFERENCES `users` (`id`);
-
---
--- Constraints for table `posts_tags`
---
-ALTER TABLE `posts_tags`
-  ADD CONSTRAINT `posts_tags_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `posts_tags_ibfk_2` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`id`) ON DELETE CASCADE;
-
---
--- Constraints for table `rsvps`
---
-ALTER TABLE `rsvps`
-  ADD CONSTRAINT `rsvps_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `events` (`id`),
-  ADD CONSTRAINT `rsvps_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
