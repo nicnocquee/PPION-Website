fde2b48dbbef1fff38dad9b1ca58b054234a4042
Nico Prananta <nico@flutterscape.com>
2011-11-06 18:20:35 +0900

    added home

--- application/views/home.php
+++ application/views/home.php
@@ -1,24 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<title>Home</title>
-	<link rel="stylesheet" href="<?php echo base_url(); ?>css/style.css"
-		type="text/css" media="all">
-</head>
-<body>
-
-<div>
-
-	<?php if(models\Current_User::user()): ?>
-		<h2>Hello <em><?php echo models\Current_User::user()->getName(); ?></em>.</h2>
-		<h2><?php echo anchor('logout','Logout'); ?></h2>
-	<?php else: ?>
-		<h2>New Users: <?php echo anchor('signup','Create an Account'); ?>.</h2>
-		<h2>Members: <?php echo anchor('login','Login'); ?>.</h2>
-	<?php endif; ?>
-
+<div id="home" class="container">
+	<div class="row">
+		<div class="span16"  id="flickr">
+			<center><a href="http://www.flickr.com/photos/ppi-on/5610026711/" title="hanami2011-2 by PPI-ON, on Flickr"><img src="http://farm6.static.flickr.com/5224/5610026711_f7b4925fb0_b.jpg" height="400" alt="hanami2011-2"></a></center>
+		</div>
+	</div>
+	<div class="row">
+		<div class="span-one-third">
+		<div id="about">
+			<? include("static/aboutppion.html"); ?>
+		</div>
+		</div>
+		<div class="span-one-third">
+			<div id="recent_posts">
+			<h2>Artikel terbaru</h2>
+			<ul class="newposts">
+			<?php  
+				foreach ($posts as $post) {
+					echo '<li style="margin-left:-25px"><a href="/posts/'.$post->getId().'">'.$post->getTitle().'</a></li>';
+				}
+			?>
+			</ul>
+			</div>
+		</div>
+		<div class="span-one-third">
+		<div id="recent_events">
+			<h2>Kegiatan terbaru</h2>
+			<ul class="newposts">
+			<?php  
+				foreach ($events as $event) {
+					echo '<li style="margin-left:-25px"><a href="/events/'.$event->getId().'">'.$event->getName().'</a></li>';
+				}
+			?>
+			</ul>
+			</div>
+		</div>
+		</div>
+	</div>
 </div>
-
-</body>
-</html>
