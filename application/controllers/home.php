f9a2444781de903107b2782eab087d51bbfd2dd7
Nico Prananta <nico@flutterscape.com>
2011-12-29 01:57:16 +0900

    in home show the not deleted posts

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -8,14 +8,14 @@ class Home extends MY_Controller {
 	}
             
     function index() {
-	    $q = $this->em->createQuery("select p from models\Post p");
+	    $q = $this->em->createQuery("select p from models\Post p where p.flag is null");
 	    $q->setMaxResults(9);
     	$recentPosts = $q->getResult();
-    	$q = $this->em->createQuery("select e from models\Event e");
-    	$q->setMaxResults(9);
-    	$recentEvents = $q->getResult();
+    	//$q = $this->em->createQuery("select e from models\Event e");
+    	//$q->setMaxResults(9);
+    	//$recentEvents = $q->getResult();
 		$data['posts'] = $recentPosts;
-		$data['events'] = $recentEvents;
+		$data['events'] = array();
 		//$this->load->view('home', $data);
 		$this->template->build('home', $data);
 		
